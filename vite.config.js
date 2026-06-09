e6dad78f7c35897c8300962c627640a24b0398da
Reihan Fahlevi <whenautumnend@gmail.com>
2026-06-09 22:30:10 +0700

    feat: add gh pages deployment workflow

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from "@vitejs/plugin-react";
 import { viteSingleFile } from "vite-plugin-singlefile";
 
 export default defineConfig({
+  base: "/homecoming/",
   plugins: [react(), viteSingleFile()],
 });
